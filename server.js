50d9cf18eb250df3a249911465406c6fb7771a18
Benbodhi <5193411+benbodhi@users.noreply.github.com>
2023-04-28 06:19:09 +1000

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const Web3 = require('web3');
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server listening on port ${port}`));
 
 app.use(cors());
 app.use(express.static('public'));
